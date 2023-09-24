656597ae868088055810428a54a7b020978bd3fd
Scott Rallya <srallya@gmail.com>
2023-09-24 10:32:42 -0400

    Rust configs

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -19,9 +19,14 @@ local protocol = require("vim.lsp.protocol")
 end
 --]]
 
+nvim_lsp.denols.setup({
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+})
+
 -- TypeScript
 nvim_lsp.tsserver.setup({
-  on_attach = on_attach,
+  --  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("package.json"),
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
 })
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,13 @@
 return {
   "neovim/nvim-lspconfig",
-  "simrat39/rust-tools.nvim",
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require("config.rust-tools")
+    end,
+  },
 
   -- Complete and Snippets plugin suite
   "hrsh7th/cmp-buffer",
@@ -56,4 +63,20 @@ return {
   "L3MON4D3/LuaSnip",
   "akinsho/nvim-bufferline.lua",
   "glepnir/lspsaga.nvim",
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
 }
